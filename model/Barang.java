fb4dc15dd28f0ab722a03d96c38e26f0711362bb
muhammad13-droid <rifaimuh13@gmail.com>
2025-11-15 20:42:07 +0800

    Menggunakan library org.json (json-20230517.jar)

--- model/Barang.java
+++ model/Barang.java
@@ -25,7 +25,7 @@ public class Barang {
         this.kategori = kategori;
     }
 
-    // --- Getters & Setters ---
+    // --- Getters & Setters --- //
     public String getKode() { return kode; }
     public void setKode(String kode) { this.kode = kode; }
     public String getNama() { return nama; }
